02119930ac854052101aad99fe437c6212fe35d5
ronen.goren <ronen.goren@gmail.com>
2018-07-25 00:57:45 -0400

    added linkdin auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,7 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
-
+  config.secret_key = 'd5a19edc00626a655bd63ccb57534851ae85ae1d983c3943c33b107c723ae42b1f7f49956f894f9dea58ea62491100450752cef81f35e9b5ced32f3fa890e146'
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
